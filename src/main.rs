dcf0a3ddf4f2956b66e109add22a8b831ae50f51
Muhammad Mominul Huque <mominul2082@gmail.com>
2023-01-28 06:45:40 +0600

    CORS is our friend now at last and logger is enabled

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use axum_project::app;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
+
     // run it with hyper on localhost:3000
     axum::Server::bind(&"127.0.0.1:8000".parse().unwrap())
         .serve(app(false).await.into_make_service())
